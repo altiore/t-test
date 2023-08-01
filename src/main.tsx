e0122ff32297437bce88e05d48f7acdb4b37da75
Razzwan <razvanlomov@gmail.com>
2023-08-01 09:27:46 +0300

    feat(publish): prepare for publish

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,8 @@ import App from './App.tsx'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-// this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <TonConnectUIProvider manifestUrl={manifestUrl}>
+  <TonConnectUIProvider manifestUrl={'https://altiore.github.io/t-test/tonconnect-manifest.json'}>
     <App />
   </TonConnectUIProvider>
 )
